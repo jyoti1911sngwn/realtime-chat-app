1bb19a02e99ad4badda5f0f5a983dfbd7a8327d3
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-12-03 11:31:26 +0530

    router import

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,19 +1,22 @@
 import express from "express"
-
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js";
 const app = express();
-const PORT = "3000"
 
-app.get("/api/auth/signup" , (req, res)=>{
+dotenv.config();
+const PORT = process.env.PORT || 5000 
+
+authRoutes.get("/api/auth/signup" , (req, res)=>{
     res.send("User has signed up")
 })
-app.get("/api/auth/login" , (req, res)=>{
+authRoutes.get("/api/auth/login" , (req, res)=>{
     res.send("User has logged in")
 })
-app.get("/api/auth/logout" , (req, res)=>{
+authRoutes.get("/api/auth/logout" , (req, res)=>{
     res.send("User has logged out")
 })
 
 
 app.listen(PORT , ()=>{
-    console.log("listening to app")
+    console.log(`listening to app on port ${PORT}`)
 })
