eba0d85608f885966df7091d6af183aa89df6b89
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-12-03 13:08:08 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,26 +1,26 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import path from "path";
 
-const app = express();
-
 dotenv.config();
-const PORT = process.env.PORT || 5000 
+const app = express();
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-// ALWAYS SERVE FRONTEND BUILD ON RENDER
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`listening to app on port ${PORT}`);
 });
-
-
-app.listen(PORT , ()=>{
-    console.log(`listening to app on port ${PORT}`)
-})
